3a7ee765b78077b2a750cfdf904d025da5a40ed8
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-12 23:34:33 +0800

    Remove and update test

--- test/api/users/get_users.spec.js
+++ test/api/users/get_users.spec.js
@@ -6,51 +6,24 @@ describe('Routes: GET Users', () => {
 
   const UserUseCase = models('users')
 
-  const signIn = app.resolve('jwt').signin()
-  let token
-
   beforeEach((done) => {
     // we need to add user before we can request our token
     UserUseCase
       .destroy({ where: {} })
       .then(() =>
         UserUseCase.create({
-          firstName: 'Test',
-          lastName: 'Dev',
-          middleName: 'Super Dev',
-          email: 'testdev1@gmail.com',
-          password: 'pass',
-          roleId: 1,
-          isDeleted: 0,
-          createdBy: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b'
+          name: 'Super Dev'
         })
       ).then(() =>
         UserUseCase.create({
-          firstName: 'John',
-          lastName: 'doe',
-          middleName: 'JohnDoe',
-          email: 'superjohndoe@gmail.com',
-          password: 'pass',
-          roleId: 1,
-          isDeleted: 0,
-          createdBy: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b'
+          name: 'John'
         })
-      ).then((user) => {
-        token = signIn({
-          id: user.id,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          middleName: user.middleName,
-          email: user.email
-        })
-        done()
-      })
+      ).then(() => done())
   })
 
   describe('Should return users', () => {
     it('should return all users', (done) => {
       request.get(`${BASE_URI}/users`)
-        .set('Authorization', `JWT ${token}`)
         .expect(200)
         .end((err, res) => {
           expect(res.body.data).to.have.length(2)
