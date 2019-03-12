118adb0a3771faf359fbd137c2559b2faa79e1bf
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-12 22:34:48 +0800

    Should inject function not value

--- src/container.js
+++ src/container.js
@@ -27,7 +27,7 @@ container
     response: asFunction(response).singleton(),
     date: asFunction(date).singleton(),
     config: asValue(config),
-    repository: asFunction(repository),
+    repository: asFunction(repository)
   })
 
 module.exports = container
