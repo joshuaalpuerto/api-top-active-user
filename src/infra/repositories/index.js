dcd31a47eff9e776c06b5134563c675086596d0e
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-13 22:44:09 +0800

    Expose connections to respository service

--- src/infra/repositories/index.js
+++ src/infra/repositories/index.js
@@ -1,8 +1,21 @@
-const UserRepository = require('./user')
+const User = require('./user')
+const Company = require('./company')
+const Listing = require('./listing')
+const Team = require('./team')
+const Application = require('./application')
 
 module.exports = ({ database }) => {
   const userModel = database.models.users
+  const companyModel = database.models.companies
+  const listingModel = database.models.listings
+  const teamModel = database.models.teams
+  const applicationModel = database.models.applications
+
   return {
-    userRepository: UserRepository({ model: userModel })
+    userRepository: User({ model: userModel }),
+    companyRepository: Company({ model: companyModel }),
+    listingRepository: Listing({ model: listingModel }),
+    teamRepository: Team({ model: teamModel }),
+    applicationRepository: Application({ model: applicationModel })
   }
 }
