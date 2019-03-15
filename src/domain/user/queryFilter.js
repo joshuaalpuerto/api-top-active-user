4a08693be50cc4f689331e3cd937e5fe4ce32536
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-15 10:29:47 +0800

    Adjust domain handler for queryParams

--- src/domain/user/queryFilter.js
+++ src/domain/user/queryFilter.js
@@ -2,12 +2,18 @@ const t = require('tcomb')
 const { compose, multiply } = require('ramda')
 const { cleanData } = require('../helper')
 
+/**
+ * Accept string instead of number
+ * due to nature of querystring wrapped always to string
+ */
 const UserParams = t.struct({
-  page: t.maybe(t.Number)
+  page: t.maybe(t.String),
+  userId: t.maybe(t.String)
 })
 
 UserParams.prototype.format = function () {
-  this.page = multiply(5, this.page)
+  this.page = multiply(5, this.page || 0)
+  this.userId = this.userId ? parseInt(this.userId) : null
 
   return this
 }
